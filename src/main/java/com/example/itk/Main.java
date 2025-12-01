e3dd6b6fedc3a6e9876c1082b109358656785882
Maiia Vyletok <mvyletok@devexperts.com>
2025-12-01 09:58:22 +0300

    -- maven

--- src/main/java/com/example/itk/Main.java
+++ src/main/java/com/example/itk/Main.java
@@ -1,7 +1,12 @@
 package com.example.itk;
 
+import com.example.geometry.Rectangle;
+import com.example.geometry.Shape;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Shape rectangle = new Rectangle(10, 3);
+        System.out.println("rectangle perimeter is: " + rectangle.getPerimeter());
+        System.out.println("rectangle area is: " + rectangle.getArea());
     }
 }
